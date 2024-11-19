30c96f0bb639321b9d1b4479c09f46a64328c1bd
haruki7049 <tontonkirikiri@gmail.com>
2024-11-20 07:11:22 +0900

    Move libleak.nix to default.nix

--- default.nix
+++ default.nix
@@ -1,1 +1,13 @@
-(import <nixpkgs> { }).callPackage ./libleak.nix { }
+{ stdenv, libbacktrace }:
+
+stdenv.mkDerivation {
+  name = "libleak";
+
+  src = ./.;
+  buildInputs = [ libbacktrace ];
+
+  installPhase = ''
+    mkdir -p $out/lib
+    cp libleak.so $out/lib
+  '';
+}
